52b6ed906fd6025747798a30b07d5fd781d8b1c9
galiev_mr <galiev_mr@taximaxim.ru>
2017-09-29 11:39:25 +0300

    added oid to view

--- DATABASE/SCHEMA/public/VIEW/show_objects.sql
+++ DATABASE/SCHEMA/public/VIEW/show_objects.sql
@@ -1,11 +1,12 @@
 SET search_path = public, pg_catalog;
 
 CREATE VIEW show_objects AS
-    SELECT f.type,
-    f.schema,
-    f.name,
-    f.identity,
-    t.last_modified
+    SELECT 
+            t.objid,
+            f.type,
+            f.schema,
+            f.name,
+            f.identity,
+            t.last_modified
    FROM ddl_events t,
-    LATERAL pg_identify_object(t.classid, t.objid, t.objsubid) f(type, schema, name, identity);
-
+            LATERAL pg_identify_object(t.classid, t.objid, t.objsubid) f(type, schema, name, identity);
