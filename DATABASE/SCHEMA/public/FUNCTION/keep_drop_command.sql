b2c3d1633e651c0581d5573ba8e9a4e3c851e016
galiev_mr <galiev_mr@taximaxim.ru>
2017-10-09 10:51:31 +0300

    fixed error with cascade drop

--- DATABASE/SCHEMA/public/FUNCTION/keep_drop_command.sql
+++ DATABASE/SCHEMA/public/FUNCTION/keep_drop_command.sql
@@ -3,22 +3,17 @@ SET search_path = public, pg_catalog;
 CREATE OR REPLACE FUNCTION keep_drop_command() RETURNS event_trigger
     LANGUAGE plpgsql
     AS $$
-	DECLARE
-		old_classid	oid;
-		old_objid	oid;
-		old_objsubid	integer;
-		is_temp bool;
-	BEGIN
-		SELECT classid, objid, objsubid, is_temporary
-		INTO old_classid, old_objid, old_objsubid, is_temp
-		FROM pg_event_trigger_dropped_objects();
-
-		IF NOT is_temp 
-		THEN
-			DELETE FROM ddl_events 
-			WHERE classid = old_classid 
-				AND objid = old_objid 
-				AND objsubid = old_objsubid;
-		END IF;
-	END;
+    DECLARE
+        r record;
+    BEGIN
+        FOR r IN SELECT * FROM pg_event_trigger_dropped_objects() LOOP 
+            IF NOT r.is_temporary 
+            THEN
+                DELETE FROM ddl_events 
+                WHERE classid = r.classid 
+                    AND objid = r.objid 
+                    AND objsubid = r.objsubid;
+            END IF; 
+        END LOOP;
+    END;
 $$;
