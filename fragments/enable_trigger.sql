3f739f80df6706916476a41593f3c871177d5523
galiev_mr <galiev_mr@taximaxim.ru>
2018-02-07 15:25:58 +0300

    updated pgCodeKeeper version

--- fragments/enable_trigger.sql
+++
@@ -1,3 +0,0 @@
-\echo Use "ALTER EXTENSION pg_dbo_timestamp UPDATE TO '0.0.2'" to load this file. \quit
-
-ALTER EVENT TRIGGER keep_ddl_timestamps ENABLE;
--- generate.sh
+++ generate.sh
@@ -2,9 +2,7 @@
 mkdir -p scripts
 unzip -o pgCodeKeeper-cli-*.zip -d generated
 ./generated/pgcodekeeper-cli.sh -o scripts/pg_dbo_timestamp--0.0.1.sql  DATABASE/ fragments/empty.sql
-sed -i '1s/^/\\\echo Use "CREATE EXTENSION pg_dbo_timestamp" to load this file. \\\quit\n\n\n/' scripts/pg_dbo_timestamp--0.0.1.sql
+var='\\\echo Use "CREATE EXTENSION pg_dbo_timestamp" to load this file. \\\quit'
+sed -i "1s/.*/$var/" scripts/pg_dbo_timestamp--0.0.1.sql
 cat fragments/disable_trigger.sql >> scripts/pg_dbo_timestamp--0.0.1.sql
 sed -i 's/ddl_events/@extschema@.ddl_events/g' scripts/pg_dbo_timestamp--0.0.1.sql
-cat scripts/pg_dbo_timestamp--0.0.1--0.0.2.sql
->scripts/pg_dbo_timestamp--0.0.1--0.0.2.sql
-cat fragments/enable_trigger.sql >> scripts/pg_dbo_timestamp--0.0.1--0.0.2.sql
