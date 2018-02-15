23641b4f6072c725c9d72649275b9a98145d2eab
Левша Александр <levsha_aa@taximaxim.ru>
2018-02-15 15:20:54 +0300

    event triggers should never raise exceptions

--- DATABASE/SCHEMA/public/FUNCTION/keep_any_command.sql
+++ DATABASE/SCHEMA/public/FUNCTION/keep_any_command.sql
@@ -6,6 +6,9 @@ CREATE OR REPLACE FUNCTION keep_any_command() RETURNS event_trigger
     AS $$
     DECLARE
         r record;
+        _exstate text;
+        _exmsg text;
+        _exctx text;
     BEGIN
         FOR r IN SELECT * FROM pg_event_trigger_ddl_commands() LOOP
             IF r.classid IS NOT NUll AND r.objid IS NOT NULL 
@@ -19,8 +22,15 @@ CREATE OR REPLACE FUNCTION keep_any_command() RETURNS event_trigger
                     INSERT INTO ddl_events (classid, objid) SELECT r.classid, r.objid;
                 END IF;
             ELSE 
-                RAISE WARNING 'Unsupported operation';
+                RAISE NOTICE 'DDL unsupported by pg_dbo_timestamp';
             END IF;
         END LOOP;
+    EXCEPTION WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS 
+            _exstate = RETURNED_SQLSTATE,
+            _exmsg = MESSAGE_TEXT,
+            _exctx = PG_EXCEPTION_CONTEXT;
+        RAISE WARNING 'Error in pg_dbo_timestamp event trigger function. state: %, message: %, context: %', 
+            _exstate, _exmsg, _exctx;
     END;
 $$;
--- DATABASE/SCHEMA/public/FUNCTION/keep_drop_command.sql
+++ DATABASE/SCHEMA/public/FUNCTION/keep_drop_command.sql
@@ -6,6 +6,9 @@ CREATE OR REPLACE FUNCTION keep_drop_command() RETURNS event_trigger
     AS $$
     DECLARE
         r record;
+        _exstate text;
+        _exmsg text;
+        _exctx text;
     BEGIN
         FOR r IN SELECT * FROM pg_event_trigger_dropped_objects() LOOP 
             IF NOT r.is_temporary 
@@ -15,5 +18,12 @@ CREATE OR REPLACE FUNCTION keep_drop_command() RETURNS event_trigger
                     AND objid = r.objid;
             END IF; 
         END LOOP;
+    EXCEPTION WHEN OTHERS THEN
+        GET STACKED DIAGNOSTICS 
+            _exstate = RETURNED_SQLSTATE,
+            _exmsg = MESSAGE_TEXT,
+            _exctx = PG_EXCEPTION_CONTEXT;
+        RAISE WARNING 'Error in pg_dbo_timestamp event trigger function. state: %, message: %, context: %', 
+            _exstate, _exmsg, _exctx;
     END;
 $$;
