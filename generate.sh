86d3c912bcb11deb3b6596362034905991e7bc01
Левша Александр <levsha_aa@taximaxim.ru>
2018-02-15 16:53:46 +0300

    removed initial search_path in build script

--- generate.sh
+++ generate.sh
@@ -3,5 +3,6 @@ mkdir -p scripts
 unzip -o pgCodeKeeper-cli-*.zip -d generated > /dev/null
 echo '\echo Use "CREATE EXTENSION pg_dbo_timestamp" to load this file. \quit' > scripts/pg_dbo_timestamp--0.0.1.sql
 ./generated/pgcodekeeper-cli.sh DATABASE/ fragments/empty.sql >> scripts/pg_dbo_timestamp--0.0.1.sql 
+sed -i "s/SET search_path = public, pg_catalog;//" scripts/pg_dbo_timestamp--0.0.1.sql
 sed -i "s/SET search_path = public, pg_catalog/SET search_path = @extschema@, pg_catalog/" scripts/pg_dbo_timestamp--0.0.1.sql
 cat fragments/finish_install.sql >> scripts/pg_dbo_timestamp--0.0.1.sql
