6d4b7c5d8d43db654aaa1f4b975a57d4e4b91f9b
galiev_mr <galiev_mr@taximaxim.ru>
2018-03-21 14:28:05 +0300

    added full script generation

--- generate.sh
+++ generate.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# generate full and update scripts from latest Github release
 
 if [ "$#" -ne 1 ] 
 then
@@ -9,7 +10,8 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 NEW_VERSION="$1"
 LAST_VERSION="$(git --git-dir $DIR/.git describe --tags --abbrev=0 master)"
 WORK_DIR="$(mktemp -d)"
-SCRIPT_PATH="$DIR/scripts/pg_dbo_timestamp--${LAST_VERSION:1}--$NEW_VERSION.sql"
+UPDATE_SCRIPT_PATH="$DIR/scripts/pg_dbo_timestamp--${LAST_VERSION:1}--$NEW_VERSION.sql"
+FULL_SCRIPT_PATH="$DIR/scripts/pg_dbo_timestamp--$NEW_VERSION.sql"
 
 # check temp dir creating
 if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
@@ -17,8 +19,11 @@ if [[ ! "$WORK_DIR" || ! -d "$WORK_DIR" ]]; then
   exit 1
 fi
 
-# write warning to first line of file
-echo '\echo Use "UPDATE EXTENSION pg_dbo_timestamp TO $NEW_VERSION" to load this file. \quit' > $SCRIPT_PATH
+# write warning on first line in update script
+echo '\echo Use "UPDATE EXTENSION pg_dbo_timestamp TO $NEW_VERSION" to load this file. \quit' > $UPDATE_SCRIPT_PATH
+
+# write warning on first line in full script
+echo '\echo Use "CREATE EXTENSION pg_dbo_timestamp" to load this file. \quit' > $FULL_SCRIPT_PATH
 
 (cd $WORK_DIR
 # load and unzip pgCodeKeeper cli version
@@ -26,15 +31,26 @@ wget "https://github.com/pgcodekeeper/pgcodekeeper/releases/download/v4.3.1/pgCo
 unzip -o pgCodeKeeper-cli-*.zip -d pgcodekeeper > /dev/null
 # clone last tag
 git clone $DIR --shared --branch $LAST_VERSION --single-branch  &> /dev/null
-# generate diff
-./pgcodekeeper/pgcodekeeper-cli.sh -F $DIR/DATABASE/ pg_dbo_timestamp/DATABASE >> $SCRIPT_PATH
+# generate update diff
+./pgcodekeeper/pgcodekeeper-cli.sh -F $DIR/DATABASE/ pg_dbo_timestamp/DATABASE >> $UPDATE_SCRIPT_PATH
+# generate full diff
+./pgcodekeeper/pgcodekeeper-cli.sh -F $DIR/DATABASE/ /dev/null >> $FULL_SCRIPT_PATH
 )
 
-# remove first search path from script
-sed -i "s/SET search_path = public, pg_catalog;//" $SCRIPT_PATH
+# remove first search path from update script
+sed -i "s/SET search_path = public, pg_catalog;//" $UPDATE_SCRIPT_PATH
 
-# replace search path in functions
-sed -i "s/SET search_path TO public, pg_catalog/SET search_path TO @extschema@, pg_catalog/" $SCRIPT_PATH
+# remove first search path from full script
+sed -i "s/SET search_path = public, pg_catalog;//" $FULL_SCRIPT_PATH
+
+# replace search path in functions in update script
+sed -i "s/SET search_path TO public, pg_catalog/SET search_path TO @extschema@, pg_catalog/" $UPDATE_SCRIPT_PATH
+
+# replace search path in functions in full script
+sed -i "s/SET search_path TO public, pg_catalog/SET search_path TO @extschema@, pg_catalog/" $FULL_SCRIPT_PATH
+
+# added event triggers to full script
+cat $DIR/fragments/finish_install.sql >> $FULL_SCRIPT_PATH
 
 #remove temp dir
 rm -rf "$WORK_DIR"
