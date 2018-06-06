b3dc23ba9fbbc1d08512e7ebf0573368cb490be8
Левша Александр <levsha_aa@taximaxim.ru>
2018-06-06 15:39:12 +0300

    added object ACLs to object_timestamps view

--- DATABASE/SCHEMA/public/VIEW/dbots_object_timestamps.sql
+++ DATABASE/SCHEMA/public/VIEW/dbots_object_timestamps.sql
@@ -1,15 +1,59 @@
 SET search_path = public, pg_catalog;
 
 CREATE VIEW dbots_object_timestamps AS
-	SELECT 
-            t.objid,
-            f.type,
-            f.schema,
-            f.name,
-            f.identity,
-            t.last_modified,
-            t.ses_user,
-            t.cur_user, 
-            t.ip_address
-    FROM dbots_event_data t,
-            LATERAL dbots_get_object_identity(t.classid, t.objid) f(type, schema, name, identity);
+	WITH acls AS (
+         SELECT union_acls.tableoid,
+            union_acls.oid,
+            union_acls.acl,
+            union_acls.colnames,
+            union_acls.colacls
+           FROM ( SELECT pg_proc.tableoid,
+                    pg_proc.oid,
+                    pg_proc.proacl,
+                    NULL::text[] AS text,
+                    NULL::text[] AS text
+                   FROM pg_proc
+                UNION ALL
+                 SELECT pg_namespace.tableoid,
+                    pg_namespace.oid,
+                    pg_namespace.nspacl,
+                    NULL::text[] AS text,
+                    NULL::text[] AS text
+                   FROM pg_namespace
+                UNION ALL
+                 SELECT pg_type.tableoid,
+                    pg_type.oid,
+                    pg_type.typacl,
+                    NULL::text[] AS text,
+                    NULL::text[] AS text
+                   FROM pg_type
+                UNION ALL
+                 SELECT c.tableoid,
+                    c.oid,
+                    c.relacl,
+                    attrs.attnames,
+                    attrs.attacls
+                   FROM (pg_class c
+                     LEFT JOIN ( SELECT attr.attrelid,
+                            array_agg(attr.attname ORDER BY attr.attnum) AS attnames,
+                            array_agg((attr.attacl)::text ORDER BY attr.attnum) AS attacls
+                           FROM pg_attribute attr
+                          WHERE ((attr.attnum > 0) AND (attr.attisdropped IS FALSE) AND (attr.attacl IS NOT NULL))
+                          GROUP BY attr.attrelid) attrs ON ((c.oid = attrs.attrelid)))) union_acls(tableoid, oid, acl, colnames, colacls)
+          WHERE ((union_acls.acl IS NOT NULL) OR (union_acls.colacls IS NOT NULL))
+        )
+ SELECT (a.acl)::text AS acl,
+    a.colnames,
+    a.colacls,
+    t.objid,
+    f.type,
+    f.schema,
+    f.name,
+    f.identity,
+    t.last_modified,
+    t.ses_user,
+    t.cur_user,
+    t.ip_address
+   FROM (dbots_event_data t
+     LEFT JOIN acls a ON (((a.tableoid = t.classid) AND (a.oid = t.objid)))),
+    LATERAL dbots_get_object_identity(t.classid, t.objid) f(type, schema, name, identity);
